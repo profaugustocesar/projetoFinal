f4abf422dbf3d0e27d72acf3e7412c94fafda9e3
Prof. Augusto César <oprofaugustocesar@gmail.com>
2023-12-02 11:13:18 -0300

    Jquery + Máscaras

--- admin/corretor/formCadastro.php
+++ admin/corretor/formCadastro.php
@@ -44,7 +44,7 @@
 
             <div class="mb-3">
                 <label for="txtTelefone" class="form-label">Telefone</label>
-                <input type="text" class="form-control" id="txtTelefone" name="txtTelefone">
+                <input type="tel" class="form-control" id="txtTelefone" name="txtTelefone" data-mask="(00) 0.0000-0000">
             </div>
 
             
@@ -54,6 +54,8 @@
     </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="../js/jquery.mask.min.js"></script>
 </body>
 
 </html>
